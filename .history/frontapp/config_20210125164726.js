da68f9b497e198418a8f82490d6940797dcd607b
Milenko <milenko.markovic@gmail.com>
2021-01-25 17:49:08 +0100

    Removed git rm -r --cached .history

--- .history/frontapp/config_20210125164726.js
+++
@@ -1,8 +0,0 @@
-import getConfig from 'next/config';
-const { publicRuntimeConfig } = getConfig();
-
-console.log(publicRuntimeConfig);
-export const API = publicRuntimeConfig.PRODUCTION
-    ? 'https://cryptoblog.com'
-    : 'http://localhost:5000';
-export const APP_NAME = publicRuntimeConfig.APP_NAME;
